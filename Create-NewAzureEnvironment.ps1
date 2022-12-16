458d92adf846a245e6a9e5c627fb74745fb5267d
Micaella Illescas <100733950+micaellafei@users.noreply.github.com>
2022-12-16 18:54:31 +0800

    Update Create-NewAzureEnvironment.ps1

--- Create-NewAzureEnvironment.ps1
+++ Create-NewAzureEnvironment.ps1
@@ -1,25 +1,23 @@
 
 #initialization/Installation
 
-$ADgroupId = (Get-AzADGroup -DisplayName $Gname).Id
-$SPObjId = (Get-AzADServicePrincipal -ServicePrincipalName $SPname).Id
-
-#cann add other members
-$members = @()
-
-#members += (Get-AzureADUser).id
-$members += (Get-AzADServicePrincipal -ObjectId $SPObjId).Id
-
-
 # Create the Azure resources in our tenant:
 
-New-AzADGroup -DisplayName $gname -SecurityEnabled    #create ad security group
-New-AzADServicePrincipal -DisplayName  $spname        #create service principal
+New-AzADGroup -DisplayName "$gname" -SecurityEnabled    #create ad security group
+New-AzADServicePrincipal -DisplayName  "$spname"        #create service principal
+
+$ADgroupId = (Get-AzADGroup -DisplayName "$gname").Id
+$SPObjId = (Get-AzADServicePrincipal -ServicePrincipalName "$spname").Id
+
+
+$members = @()    #cann add other members
+#members += (Get-AzureADUser).id
+$members += (Get-AzADServicePrincipal -ObjectId $SPObjId).Id
 
 Add-AzADGroupMember -TargetGroupObjectId $ADgroupId -MemberObjectId $members  #add SP in security group
 
 #create federated credential ; differs by environment variables
-New-AzADAppFederatedCredential -ApplicationObjectId $SPObjId -Audience $audience -Issuer $issuer -name $name -Subject $subject
+New-AzADAppFederatedCredential -ApplicationObjectId $SPObjId -Audience "$audience" -Issuer "$issuer" -name "$name" -Subject "$subject"
 
 # Create Service Principal in Customer Tenant
 #Connect-AzAccount –tenantId $ {{secrets.AZ_CLIENT_TENANTID}}
