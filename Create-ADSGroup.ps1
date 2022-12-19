9bfbb718db1346b76f81abb0a1365549076e5e4a
Micaella Illescas <100733950+micaellafei@users.noreply.github.com>
2022-12-19 15:09:43 +0800

    Update Create-ADSGroup.ps1

--- Create-ADSGroup.ps1
+++ Create-ADSGroup.ps1
@@ -1,13 +1,13 @@
 param($GroupDisplayName, $TenantID, $SPDisplayName)
 Connect-AzAccount -tenantid $TenantID
 
-New-AZADGroup -DisplayName $GroupDisplayName -SecurityEnabled
+New-AZADGroup -DisplayName "$GroupDisplayName" -SecurityEnabled
 
-New-AZADServicePrincipal -DisplayName $SPDisplayName
+New-AZADServicePrincipal -DisplayName "$SPDisplayName"
 
-$SPOjbID = (Get-AZADServicePrincipal -ServicePrincipalName $SPDisplayName).Id
+$SPOjbID = (Get-AZADServicePrincipal -ServicePrincipalName "$SPDisplayName".Id
 $members += @()                            #can add other members
 $members += (Get-AZADServicePrincipal -ObjectId $SPObjID).Id
-$ADgroupId = (Get-AzADGroup -DisplayName $GroupDisplayName).Id
+$ADgroupId = (Get-AzADGroup -DisplayName "GroupDisplayName").Id
 
 Add-AZADGroupMember -TargetGroupObjectId $ADgroupId -MemberObjectId $members
